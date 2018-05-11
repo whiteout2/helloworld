8f649b6a688fe02ce1ae37d98b57715575824b25
rgosens2 <39185953+rgosens2@users.noreply.github.com>
2018-05-11 10:52:54 +0200

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// TEST: color me purple
+class MyClass {
+    string str = "qqq";
+    char* str2 = "www";
+    int num = 777;
+
+    class MyClass2 {
+        int test = 33;
+    };
+
+};
+
+
+int main()
+{
+    string str1 = "QQQ";
+
+    MyClass test6;
+    
+    int test = 0;
+    cout << "Hello, world!" << endl;
+
+    test += 666;
+
+    // TEST: digit separators
+    int test2 = 1'000'000;
+    cout << "Test: " << test << endl;
+    //cerr << "Test cerr: " << test << endl;
+
+	//qqq
+
+   return 0;
+}
+
